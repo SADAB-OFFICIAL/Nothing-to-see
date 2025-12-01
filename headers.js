5cec61c9d25c969c86077bfd946830a3b05f96d9
SADAB ALI <sadabgithub@gmail.com>
2025-12-01 17:37:48 +0530

    Create headers.js

---
+++ headers.js
@@ -0,0 +1,8 @@
+// headers.js
+module.exports = {
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+};
